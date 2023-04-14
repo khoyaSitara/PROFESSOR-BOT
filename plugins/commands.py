e0569ae814a286d6083032060c63e1c9d25be237
Ashish Sharma <99271761+AshishSharma2006@users.noreply.github.com>
2023-04-13 20:34:08 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA")
+        m=await message.reply_sticker("CAACAgUAAxkBAAEIkqRkOMjiqQWKtugXAWG1onMwjdUvaQAC0wYAAuXVsFTCTPPFFgfD9C8E")
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
@@ -98,7 +98,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action(enums.ChatAction.TYPING)
-        m=await message.reply_sticker("CAACAgUAAxkBAAIFNGJSlfOErbkSeLt9SnOniU-58UUBAAKaAAPIlGQULGXh4VzvJWoeBA")
+        m=await message.reply_sticker("CAACAgUAAxkBAAEIkqRkOMjiqQWKtugXAWG1onMwjdUvaQAC0wYAAuXVsFTCTPPFFgfD9C8E")
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
